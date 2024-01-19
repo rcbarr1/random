dbcc6f19e5d9a9bd89bda892733fdfd31dd548c4
Reese Barrett <rcbarr1@uw.edu>
2024-01-19 15:28:39 -0800

    added data visualizations for report

--- amath582_hw1.py
+++ amath582_hw1.py
@@ -9,16 +9,12 @@ Code for AMATH 582 Homework 1 assignment.
 """
 
 import numpy as np
-#import plotly
 import matplotlib.pyplot as plt
-import plotly.io as pio
-import plotly.graph_objs as go
-pio.renderers.default='browser'
 
 # load in submarine data
 filepath = '/Users/Reese/Documents/Research Projects/random/amath582/'
 subdata = np.load(filepath + 'subdata.npy')
-  
+
 #%% task 1: through averaging of the Fourier transform and visual inspection,
 # determine the frequency signature (center frequency, in kx, ky, kz)
 # determined by the submarine
@@ -31,11 +27,12 @@ x_grid = xx[0:N_grid] # spatial grid in x-direction
 y_grid = xx[0:N_grid] # spatial grid in y-direction
 z_grid = xx[0:N_grid] # spatial grid in z-direction
 
-# define frequency domain for x, y, and z dimensions - CHECK SCALING ON THIS
+# define frequency domain for x, y, and z dimensions
 Kx_grid = (2*np.pi / (2/L)) * np.linspace(-N_grid/2, N_grid/2 - 1, N_grid) # frequency grid in x-direction
 Ky_grid = (2*np.pi / (2/L)) * np.linspace(-N_grid/2, N_grid/2 - 1, N_grid) # frequency grid in y-direction
 Kz_grid = (2*np.pi / (2/L)) * np.linspace(-N_grid/2, N_grid/2 - 1, N_grid) # frequency grid in z-direction
 
+# alternative scaling method
 #Kx_grid = np.linspace(-N_grid/2, N_grid/2 - 1, N_grid) # frequency grid in x-direction
 #Ky_grid = np.linspace(-N_grid/2, N_grid/2 - 1, N_grid) # frequency grid in y-direction
 #Kz_grid = np.linspace(-N_grid/2, N_grid/2 - 1, N_grid) # frequency grid in z-direction
@@ -66,14 +63,78 @@ avg_fhat_ss_abs = np.reshape(avg_fhat_ss_abs, (N_grid, N_grid, N_grid))
 center_freq_idx = np.where(avg_fhat_ss_abs == avg_fhat_ss_abs.max()) # find index of center frequency
 center_freq = [Kx_grid[center_freq_idx[0][0]], Ky_grid[center_freq_idx[1][0]], Kz_grid[center_freq_idx[2][0]]] # convert to kx, ky, kz grid
 
-# visual inspection: plot averaged fourier domain
-xv, yv, zv = np.meshgrid(Kx_grid, Ky_grid, Kz_grid) # create meshgrid in 3D
-flat_avg_fhat_ss_abs = avg_fhat_ss_abs.flatten() # flatten avg_fhat_ss_abs
-flat_norm_avg_fhat_ss_abs = flat_avg_fhat_ss_abs / flat_avg_fhat_ss_abs.max() # normallize flat_avg_fhat_ss_abs
-fig_data = go.Isosurface(x = xv.flatten(), y = yv.flatten(), z = zv.flatten(),
-                         value = flat_norm_avg_fhat_ss_abs, isomin=0.7, isomax=0.7)
-fig = go.Figure(data = fig_data)
-fig.show()
+#%% visualize high frequency data in spatial domain
+# set up figure
+fig = plt.figure(figsize=(4,4))
+ax = fig.add_subplot(111, projection = '3d')
+
+# preallocate array
+subdata_abs_norm= np.zeros_like(np.abs(subdata))
+
+for i in range (0, subdata.shape[1]):
+    # normalize data
+    subdata_abs_norm[:,i] = np.abs(subdata[:,i]) / np.abs(subdata[:,i]).max()
+
+# save data over a threshold
+subdata_thresh = subdata_abs_norm
+all_subdata_thresh = np.sum(subdata_thresh, axis=1)
+all_subdata_thresh = all_subdata_thresh / all_subdata_thresh.max() # normalize again
+all_subdata_thresh[all_subdata_thresh < 0.80] = np.nan
+all_subdata_thresh[all_subdata_thresh >= 0.80] = 10
+all_subdata_thresh = np.reshape(all_subdata_thresh, (N_grid, N_grid, N_grid))
+
+# preallocate array to store data
+plot_sub_freq_data = np.zeros([subdata.shape[0], 4])
+
+# loop through and to create array for scatter plot
+count = 0
+for i in range(0, len(x_grid)): # loop through x-dimension
+    for j in range(0, len(y_grid)): # loop through y-dimension
+        for k in range(0, len(z_grid)): # loop through z-dimension
+            plot_sub_freq_data[count] = [x_grid[i], y_grid[j], z_grid[k], all_subdata_thresh[i, j, k]]
+            count += 1
+
+scpl = ax.scatter3D(plot_sub_freq_data[:,1], plot_sub_freq_data[:,0], plot_sub_freq_data[:,2], c=plot_sub_freq_data[:,3], alpha = 0.3) # swapped x and y here to resemble submarine.gif
+
+ax.axes.set_xlim3d(left=y_grid.min(), right=y_grid.max()) # swapped x and y here to resemble submarine.gif
+ax.axes.set_ylim3d(bottom=x_grid.min(), top=x_grid.max()) # swapped x and y here to resemble submarine.gif
+ax.axes.set_zlim3d(bottom=z_grid.min(), top=z_grid.max()) 
+
+ax.set_title('High Frequency Measurements')
+ax.set_xlabel('y') # swapped x and y here to resemble submarine.gif
+ax.set_ylabel('x') # swapped x and y here to resemble submarine.gif
+ax.set_zlabel('z')
+ax.invert_yaxis() # invert to resemble submarine.gif
+
+#%% visual inspection: plot averaged fourier domain
+# set up figure
+fig = plt.figure(figsize=(4.4,4.4))
+ax = fig.add_subplot(111, projection = '3d')
+
+# save highest 0.6 threshold of frequency data
+center_freq_idx_thresh = np.where(avg_fhat_ss_abs/avg_fhat_ss_abs.max() >= 0.6)
+center_freq_thresh = [Kx_grid[center_freq_idx_thresh[0]], Ky_grid[center_freq_idx_thresh[1]], Kz_grid[center_freq_idx_thresh[2]]] # convert to kx, ky, kz grid
+
+# set up location 
+x_coords = center_freq_thresh[0]
+y_coords = center_freq_thresh[1]
+z_coords = center_freq_thresh[2]
+
+ax.scatter3D(y_coords, x_coords, z_coords, alpha=0.01,color='steelblue',label='_nolegend_') # swapped x and y here to resemble submarine.gif
+ax.scatter3D(center_freq[1], center_freq[0], center_freq[2], alpha=0.3,color='steelblue',label='Normalized Frequency > 0.6') # swapped x and y here to resemble submarine.gif
+ax.scatter3D(center_freq[1], center_freq[0], center_freq[2],color='black',marker='x',label='Center Frequency')
+
+ax.axes.set_xlim3d(left=Ky_grid.min(), right=Ky_grid.max()) # swapped x and y here to resemble submarine.gif
+ax.axes.set_ylim3d(bottom=Kx_grid.min(), top=Kx_grid.max()) # swapped x and y here to resemble submarine.gif
+ax.axes.set_zlim3d(bottom=Kz_grid.min(), top=Kz_grid.max()) 
+
+ax.set_title('Locating Center Frequency in Three Dimensions')
+ax.set_xlabel('$k_{y}$') # swapped x and y here to resemble submarine.gif
+ax.set_ylabel('$k_{x}$') # swapped x and y here to resemble submarine.gif
+ax.set_zlabel('$k_{z}$')
+ax.invert_yaxis() # invert to resemble submarine.gif
+fig.legend(loc='lower right')
+
 
 #%% task 2: design and implement a filter to extract this frequency signature
 # in order to denoise the data and determine a more robust path of the
@@ -113,6 +174,41 @@ for i in range(0, len(Kx_grid)): # loop through x-dimension
 all_f_clean = np.zeros_like(np.abs(subdata)) # IFFT results
 all_location_idx = np.zeros([subdata.shape[1], np.ndim(Gaussian_filter)]) # submarine location at each time step
 
+#%% plot Gaussian filter
+# set up figure
+fig = plt.figure()
+ax = fig.add_subplot(111, projection = '3d')
+
+# set up location 
+# preallocate array to store data
+plot_filter_data = np.zeros([4096, 4])
+
+# loop through and sample at every 4 points
+count = 0
+for i in range(0, len(Kx_grid),4): # loop through x-dimension
+    for j in range(0, len(Ky_grid),4): # loop through y-dimension
+        for k in range(0, len(Kz_grid),4): # loop through z-dimension
+            plot_filter_data[count] = [Kx_grid[i], Ky_grid[j], Kz_grid[k], Gaussian_filter[i, j, k]]
+            count += 1
+
+scpl = ax.scatter3D(plot_filter_data[:,1], plot_filter_data[:,0], plot_filter_data[:,2], c=plot_filter_data[:,3], alpha = 0.15) # swapped x and y here to resemble submarine.gif
+ax.scatter3D(center_freq[1], center_freq[0], center_freq[2],color='black',marker='x',label='Center Frequency',s=20)
+
+c = fig.colorbar(scpl, ax=ax, pad=0.15)
+c.set_label('Value of Gaussian Filter')
+
+ax.axes.set_xlim3d(left=Ky_grid.min(), right=Ky_grid.max()) # swapped x and y here to resemble submarine.gif
+ax.axes.set_ylim3d(bottom=Kx_grid.min(), top=Kx_grid.max()) # swapped x and y here to resemble submarine.gif
+ax.axes.set_zlim3d(bottom=Kz_grid.min(), top=Kz_grid.max()) 
+
+ax.set_title('Heat Map of Gaussian Filter')
+ax.set_xlabel('$k_{y}$') # swapped x and y here to resemble submarine.gif
+ax.set_ylabel('$k_{x}$') # swapped x and y here to resemble submarine.gif
+ax.set_zlabel('$k_{z}$')
+ax.invert_yaxis() # invert to resemble submarine.gif
+
+
+#%%
 # flatten Gaussian filter
 flat_Gaussian_filter = Gaussian_filter.flatten()
 
@@ -144,7 +240,7 @@ for i in range(0,len(all_location)):
     all_location[i,1] = y_grid[int(all_location_idx[i,1])]
     all_location[i,2] = z_grid[int(all_location_idx[i,2])]
 
-#%% plot actual coordinates in 3D
+# plot actual coordinates in 3D
 
 # set up figure
 fig = plt.figure()
@@ -197,31 +293,5 @@ ax.set_ylim([-10, 10])
 ax.set_xlabel('x')
 ax.set_ylabel('y',rotation=0)
 ax.set_title("Submarine Trajectory (Top-Down View)")
-
-#%% bonus: implement an alternative technique for noise filtering,
-# substantially different from task 2, and visualize and compare the results
-# for this, basic assumption is changing: I will no longer assume the
-# submarine's frequency is constant in time
-
-# implement wavelet transformation (slide window, larger window at low
-# frequencies and smaller window at high frequencies)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
